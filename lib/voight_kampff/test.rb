6a020cefd6b0f1de6576680b5bcb8ec7fbbb4c8c
Subhamoy Karmakar <subhamoy.karmakar@tylertech.com>
2023-04-28 13:12:21 -0700

    EN-59438: replaced exists? with exist?

--- lib/voight_kampff/test.rb
+++ lib/voight_kampff/test.rb
@@ -40,7 +40,7 @@ module VoightKampff
     end
 
     def preferred_path
-      lookup_paths.find { |path| File.exists? path }
+      lookup_paths.find { |path| File.exist? path }
     end
 
     def matching_crawler
